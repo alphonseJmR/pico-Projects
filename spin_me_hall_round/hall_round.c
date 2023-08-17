a42a49d025877d7f0b810b0de51fe5c598545304
Alphonse Reitz <131940969+alphonseJmR@users.noreply.github.com>
2023-08-17 01:45:36 -0500

    File updates
    Vscode sucks at opening the branches, so i'll just keep dragging and dropping.

--- spin_me_hall_round/hall_round.c
+++ spin_me_hall_round/hall_round.c
@@ -3,83 +3,97 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include "stdlib.h"
-#include "hardware/timer.h"
 #include "hardware/gpio.h"
 #include "hardware/adc.h"
 #include "hardware/pwm.h"
+#include "hardware/irq.h"
 
 #define hall_sensor_one 26      // Set gpio pin for hall effect sensor one.
-#define hall_sensor_two 27      // Set gpio pin for hall effect sensor two.
-#define hall_sensor_three 28    // Set gpio pin for hall effect sensor three.
 
-#define motor_engage 15         //  Engage / disengage the structure spin motor via button.
-#define motor_drive 14          //  GPIO pin related to the motor output pin.
+#define clk 1                   // Rotary encoder pin clk
+#define dt 2                    // Rotary encoder pin dt
+#define rotEnc_button 0         //  Rotary encoder button pin
+#define buzzer 3
 
-#define rgb_r 11
-#define rgb_g 12
-#define rgb_b 13
+#define rgb_r 13
+#define rgb_g 14
+#define rgb_b 15
+
+int rgbColor_Select = 0;          //  Current position of rgb led pin output.
+volatile int rotCounter;
+
 
 typedef struct hall_data{
-    uint16_t hall_one;         // Current reading of hall effect sensor 1.  adc port 0
-    long hall_one_pwm;         // Result of long map conversion of hall effect sensor 1.
-    uint16_t hall_two;         // Current reading of hall effect sensor 2.  adc port 1
-    long hall_two_pwm;         // Result of long map conversion of hall effect sensor 2.
-    uint16_t hall_three;       // Current reading of hall effect sensor 3.  adc port 2
-    long hall_three_pwm;       // Result of long map conversion of hall effect sensor 3.
-            // Button call to engage or disengage motor drive.
+    uint16_t hall_one;              //   Current reading of hall effect sensor 1.  adc port 0
+    long hall_one_pwm;              //   Result of long map conversion of hall effect sensor 1.
+    uint16_t rot_a_read;
+    long rot_a_pwm;
+    uint16_t rot_b_read;
+    long rot_b_pwm;
 } hall_data;
 hall_data hall;
 
-//  Struct to hold values for rgb data.
-typedef struct rgb_data {
-    uint8_t r;
-    uint8_t g;
-    uint8_t b;
-}rgb_data;
-rgb_data rgbColors;
-
-
+typedef struct rotValues{
+    volatile bool lastClk;
+    volatile bool Clk;
+    volatile bool lastDt;
+    volatile bool Dt;
+    volatile bool Button;
+    volatile int a;
+    volatile int b;
+}rotValues;
+rotValues rot;
 
 // Function declaration area
 long map(long x, long in_min, long in_max, long out_min, long out_max) {
     return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
-
-void rgb_color() {
-
-    rgbColors.r = hall.hall_one_pwm;
-    rgbColors.g = hall.hall_two_pwm;
-    rgbColors.b = hall.hall_three_pwm;
-
-}
-
+//  Function call to adc pins.  All three can be read from this function, provided they're replaced in the brackets.
 void adc_data_call() {
 
     adc_select_input(0);
     hall.hall_one = adc_read();
-    hall.hall_one_pwm = map(hall.hall_one, 0, 4095, 0, 4095);
-
+    hall.hall_one_pwm = map(hall.hall_one, 0, 4095, 0, 255);
+/*
     adc_select_input(1);
-    hall.hall_two = adc_read();
-    hall.hall_two_pwm = map(hall.hall_two, 0, 4095, 0, 4095);
-             
+    hall.rot_a_read = adc_read();
+    hall.rot_a_pwm= map(hall.rot_a_read, 0, 4095, 0, 255);
+
     adc_select_input(2);
-    hall.hall_three = adc_read();
-    hall.hall_three_pwm = map(hall.hall_three, 0, 4095, 0, 4095);
-            
+    hall.rot_b_read = adc_read();
+    hall.rot_b_pwm = map(hall.rot_b_read, 0, 4095, 0, 255);
+    sleep_ms(20);
+  */      
+}
+
+void color_check(int num) {
+        if(num > 4){
+
+        rgbColor_Select = 0;
+    } 
 }
 
 void button_callback(uint gpio, uint32_t events) {
-        printf("Interrupt occured at pin %d, with event %d\n", gpio, events);
 
-    if(motor_switch == false) {
-        motor_switch == true;
-    }else if(motor_switch == true){
-        motor_switch == false;
+   // printf("Interrupt occured at pin: %i, with events: %i.\n\n", gpio, events);
+    if(gpio == rotEnc_button){
+     //   printf("\n\nInterrupt occured at Button.\n\n");
+        rot.Button = true;
+        gpio_acknowledge_irq(gpio, events);
+    }
+    if(gpio == clk){
+       // printf("\n\nInterrupt occured at CLK pin.\n\n");
+        rot.Clk = gpio_get(clk);
+        gpio_acknowledge_irq(gpio, events);
+        rot.a++;
+    }
+    if(gpio == dt){
+      //  printf("\n\nInterrupt occured at DT pin.\n\n");
+        rot.Dt = gpio_get(dt);
+        gpio_acknowledge_irq(gpio, events);
+        rot.b++;
     }
 }
-
-volatile bool motor_switch;
 
 int main () {
 
@@ -89,51 +103,135 @@ int main () {
     adc_gpio_init(26);
     adc_gpio_init(27);
     adc_gpio_init(28);
-    adc_select_input(2);
 
     uint slice_num[1];
 
-    pwm_init_pin(motor_drive);
-        slice_num[1] = pwm_gpio_to_slice_num(motor_drive);
-        pwm_set_wrap(slice_num[1], 255);
-        pwm_set_chan_level(slice_num[1], PWM_CHAN_A, 0);
-        pwm_init(slice_num[1], 0, true);
-    pwm_init_pin(rgb_r);
-        slice_num[2] = pwm_gpio_to_slice_num(rgb_r);
-        pwm_set_wrap(slice_num[2], 255);
-        pwm_set_chan_level(slice_num[2], PWM_CHAN_B, 0);
-        pwm_init(slice_num[2], 0, true);
-    pwm_init_pin(rgb_g);
-        slice_num[3] = pwm_gpio_to_slice_num(rgb_g);
-        pwm_set_wrap(slice_num[3], 255);
-        pwm_set_chan_level(slice_num[3], PWM_CHAN_A, 0);
-        pwm_init(slice_num[3], 0, true);
-    pwm_init_pin(rgb_b);
-        slice_num[4] = pwm_gpio_to_slice_num(rgb_b);
-        pwm_set_wrap(slice_num[4], 255);
-        pwm_set_chan_level(slice_num[4], PWM_CHAN_B, 0);
-        pwm_init(slice_num[4], 0, true);
+        gpio_init(rgb_r);
+            gpio_set_function(rgb_r, GPIO_FUNC_PWM);
+            slice_num[1] = pwm_gpio_to_slice_num(rgb_r);
+                pwm_set_wrap(slice_num[1], 255);
+                pwm_set_chan_level(slice_num[1], PWM_CHAN_B, 0);
+                pwm_set_enabled(slice_num[1], true);
+
+        gpio_init(rgb_g);
+            gpio_set_function(rgb_g, GPIO_FUNC_PWM);
+            slice_num[2] = pwm_gpio_to_slice_num(rgb_g);
+                pwm_set_wrap(slice_num[2], 255);
+                pwm_set_chan_level(slice_num[2], PWM_CHAN_A, 0);
+                pwm_set_enabled(slice_num[2], true);
+
+        gpio_init(rgb_b);
+            gpio_set_function(rgb_b, GPIO_FUNC_PWM);
+            slice_num[3] = pwm_gpio_to_slice_num(rgb_b);
+                pwm_set_wrap(slice_num[3], 255);
+                pwm_set_chan_level(slice_num[3], PWM_CHAN_B, 0);
+                pwm_set_enabled(slice_num[3], true);
+
+        gpio_init(buzzer);
+            gpio_set_function(buzzer, GPIO_FUNC_PWM);
+            slice_num[4] = pwm_gpio_to_slice_num(buzzer);
+                pwm_set_wrap(slice_num[4], 255);
+                pwm_set_chan_level(slice_num[4], PWM_CHAN_A, 0);
+                pwm_set_enabled(slice_num[4], true);
+
+        gpio_init(rotEnc_button);
+            gpio_set_dir(rotEnc_button, GPIO_IN);
+            gpio_pull_down(rotEnc_button);
+
+        gpio_init(clk);
+            gpio_set_dir(clk, GPIO_IN);
+          //  gpio_pull_up(clk);
+
+        gpio_init(dt);
+            gpio_set_dir(dt, GPIO_IN);
+          //   gpio_pull_down(dt);
 
 
-    gpio_set_irq_enabled_with_callback(motor_switch, 0x04, 1, &button_callback);
+    gpio_set_irq_enabled_with_callback(rotEnc_button, 0x04 | 0x08, true, &button_callback);
+    gpio_set_irq_enabled_with_callback(clk, 0x04 | 0x08, true, &button_callback);
+    gpio_set_irq_enabled_with_callback(dt, 0x04 | 0x08, true, &button_callback);
+
+    rot.lastClk = gpio_get(clk);
 
 while(1){
 
+    rot.Clk = gpio_get(clk);
+
     adc_data_call();
-    rgb_color();
 
-    if(motor_switch == true) {
-        printf("Motor drive engaged.\n");
-        pwm_set_chan_level(motor_drive, PWM_CHAN_A, 127); 
-    } else {
-        printf("Motor drive disengaged.\n");
-        pwm_set_chan_level(motor_drive, PWM_CHAN_A, 0);
+    if(rot.Button == true) {
+        rgbColor_Select++;
+        rot.Button = false;
+    }else {
+       // printf("\n\nNo change to color selection.\n\n");
     }
-        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, rgbColors.r);
-        pwm_set_chan_level(slice_num[3], PWM_CHAN_A, rgbColors.g);
-        pwm_set_chan_level(slice_num[4], PWM_CHAN_A, rgbColors.b);
-        printf("RGB Colors are: R- %d, G- %d, B- %d.\n", rgbColors.r, rgbColors.g, rgbColors.b);
 
+    if(rot.Clk != rot.lastClk) {
+        if(gpio_get(dt)!= rot.Clk){
+            printf("Rotating clockwise.\n");
+            if(rotCounter < 255){
+                rotCounter++;
+                printf("Counter Incremented?\n\n");
+            }else {
+                printf("\nAt maximum rotation value.\n\n");
+            }
+            
+        }else {
+            printf("Rotating counter clockwise.\n");
+            if(rotCounter > 0){
+                rotCounter--;
+                printf("Counter Decremented?\n\n");
+            }else  {
+                printf("\nAt minimum rotation value.\n\n");
+            }
+
+        } 
+        rot.lastClk = rot.Clk;
+        rot.Dt = gpio_get(dt);
     }
-        tight_loop_contents();
+
+    printf("Hall sensor one is currently at: %ld.\n", hall.hall_one_pwm);
+    printf("Clk: %d. Dt: %d. lastClk: %d. lastDt: %d.\n\n", rot.Clk, rot.Dt, rot.lastClk, rot.lastDt);
+    printf("clk: %i.  dt: %i.\n\n", gpio_get(clk), gpio_get(dt));
+    printf("Rot Counter: %i.\n\n", rotCounter);
+    printf("Color Selected: %i.\n", rgbColor_Select);
+    printf("Encode values: %i and %i.\n\n", rot.Clk, rot.lastClk);
+    printf("Rotary interrupt amount: clk: %i, dt: %i.\n\n", rot.a, rot.b);
+    color_check(rgbColor_Select);
+
+    if(rgbColor_Select == 0) {
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, rotCounter);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, 0);
+    }else if(rgbColor_Select == 1) {
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, 0);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, rotCounter);
+    }else if(rgbColor_Select == 2){
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, 0);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, rotCounter);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, hall.hall_one_pwm);
+    }else if(rgbColor_Select == 3) {
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, rotCounter);
+    }else if(rgbColor_Select == 4) {
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, 0);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, hall.hall_one_pwm);
+        
+    }else {
+
+        printf("Error occured within the rotButton function system.\n");
+        pwm_set_chan_level(slice_num[1], PWM_CHAN_B, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[2], PWM_CHAN_A, hall.hall_one_pwm);
+        pwm_set_chan_level(slice_num[3], PWM_CHAN_B, hall.hall_one_pwm);
+    }
+    pwm_set_chan_level(slice_num[4], PWM_CHAN_A, hall.hall_one_pwm);
+    sleep_ms(hall.hall_one_pwm);
+    
+ }
+        
+tight_loop_contents();
 }
+

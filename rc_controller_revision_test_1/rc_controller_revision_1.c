f0938183e8e59f52116c108abc5b37be5da84f15
Alphonse Reitz <131940969+alphonseJmR@users.noreply.github.com>
2024-01-11 10:15:25 -0600

    Code change / update

--- rc_controller_revision_test_1/rc_controller_revision_1.c
+++ rc_controller_revision_test_1/rc_controller_revision_1.c
@@ -2,7 +2,6 @@
 #include "stdlib.h"
 #include "lcd_16x2.h"
 #include "peri_header.h"
-#include "dc_motors.h"
 #include "nrf24_driver.h"
 #include "rc_nrf_configs.h"
 #include "hardware/gpio.h"
@@ -10,39 +9,31 @@
 
 int rgb_cycle;
 
-char rf_channel[16];
-char rf_data[16];
-char rf_function[16];
-char rf_transmitting[16];
 
-  // Define the LCD pins struct for your specific configuration
-LCDPins lcd_pins = {
+rf_lcd_array_s rf_arr = {
 
-    .rs_pin = GPIO_ELEVEN,        // Replace with the GPIO pin number for RS
-    .e_pin = GPIO_TWELVE,         // Replace with the GPIO pin number for E
-    .back_light = GPIO_THIRTEEN,   // Replace with the GPIO pin number for LCD backlight.
-    .data4_pin = GPIO_SEVEN,    // Replace with the GPIO pin number for Data4
-    .data5_pin = GPIO_EIGHT,    // Replace with the GPIO pin number for Data5
-    .data6_pin = GPIO_NINE,    // Replace with the GPIO pin number for Data6
-    .data7_pin = GPIO_TEN     // Replace with the GPIO pin number for Data7
-
+  .rf_function = {' ', 'R', 'F', ' ', 'S', 'w', 'i', 't', 'c', 'h', 'i', 'n', 'g', ' ', ' '},  //  Empty [16] array for const use.
+  .rf_transmitting = {'T', 'r', 'a', 'n', 's', 'm', 'i', 't', 't', 'i', 'n', 'g', '.', '.', '.'},
+  .error_char_t =  {'C', 'h', 'a', 'n', 'n', 'a', 'l', ' ', 'E', 'r', 'r', 'o', 'r'},
+  .error_char_b = {'D', 'a', 't', 'a', ' ', 'E', 'r', 'r', 'o', 'r'}
+  
 };
 
 register_pins reg_pins = {
 
-  .register_one_data = GPIO_TWENTY,
-  .register_one_latch = GPIO_ONE,
-  .register_one_enable = GPIO_EIGHTEEN,
+  .register_one_data = GPIO_SEVEN,
+  .register_one_latch = GPIO_NINE,
+  .register_one_enable = GPIO_EIGHT,
   .register_two_data = UNDEFINED,
   .register_two_latch = UNDEFINED,
   .register_two_enable = UNDEFINED,
-  .register_clk_line = GPIO_NINETEEN
+  .register_clk_line = GPIO_THIRTEEN
 
 };
 
 button_types enabled_buttons = {
-  .rotary_button_clk = GPIO_FIFTEEN,
-  .rotary_button_dt = GPIO_SIXTEEN,
+  .rotary_button_clk = GPIO_SIXTEEN,
+  .rotary_button_dt = GPIO_FIFTEEN,
   .rotary_button_button = GPIO_SEVENTEEN,
   .analog_button_one = GPIO_FOURTEEN,
   .analog_button_two = UNDEFINED,
@@ -52,20 +43,25 @@ button_types enabled_buttons = {
 };
 
 adc_port_values pico_adc = {
+
   .adc0_pin = GPIO_TWENTY_SIX,
   .adc1_pin = GPIO_TWENTY_SEVEN,
   .adc2_pin = GPIO_TWENTY_EIGHT,
+
   .adc0_raw_read = 0,
   .adc1_raw_read = 0,
   .adc2_raw_read = 0,
+
   .adc0_max_in_map_value = 4095,
   .adc0_min_in_map_value = 0,
   .adc0_min_out_map_value = 0,
   .adc0_max_out_map_value = 255,
+
   .adc1_max_in_map_value = 4095,
   .adc1_min_in_map_value = 0,
   .adc1_min_out_map_value = 0,
   .adc1_max_out_map_value = 1024,
+
   .adc2_max_in_map_value = 4095,
   .adc2_min_in_map_value = 0,
   .adc2_min_out_map_value = 0,
@@ -76,13 +72,13 @@ interrupt_times_t callback_times = {
   .clk_max = 1500,
   .dt_max = 1500,
   .button_max = 1000,
-  .interruption_max = 700
+  .interruption_max = 1500
 };
 
 rotary_encoder_struct rotary = {
 
-  .max_rotation_value = 100,
-  .minimum_required_interrupt = 1500
+  .max_rotation_value = 500,
+  .minimum_required_interrupt = 2000
 
 };
 
@@ -98,7 +94,7 @@ pin_manager_t my_pins = {
 
 nrf_manager_t my_config = {
     // RF Channel 
-    .channel = 120,
+    .channel = 110,
 
     // AW_3_BYTES, AW_4_BYTES, AW_5_BYTES
     .address_width = AW_5_BYTES,
@@ -116,19 +112,8 @@ nrf_manager_t my_config = {
     .retr_count = ARC_10RT,
 
     // retransmission delay: ARD_250US, ARD_500US, ARD_750US, ARD_1000US
-    .retr_delay = ARD_500US 
+    .retr_delay = ARD_750US 
   };
-
-motor_flags_status cfl= {
-
-  .vdti = true,
-  .vdtn = false,
-  .vdtp = false,
-  .hdti = true,
-  .hdtl = false,
-  .hdtr = false
-
-};
 
 battery_data_t bat_status = {
 
@@ -143,6 +128,14 @@ battery_data_t bat_status = {
 
 };
 
+payloads_t load_o = {
+  .payload_zero = 0x00,
+  .payload_one_t.vertical_buffer = 0x00,
+  .payload_one_t.horizontal_buffer = 0x00,
+  .payload_two = 0x00,
+  .payload_three = 0x00
+};
+
 int main() {
 
   stdio_init_all();
@@ -152,15 +145,15 @@ int main() {
     
   adc_init();
   adc_pin_setup(&pico_adc);
+//  Initialize pin setup for rotary encoder.
   rotary_encoder_init(&enabled_buttons);
   button_interrupt_init(&enabled_buttons);
+
+//  Initialize pin setup for cd74hc595e(s).
   shift_register_pin_init(&reg_pins);
-  lcd_init(&lcd_pins);
-  lcd_4_bit_init(&lcd_pins);
-  
-    gpio_put(lcd_pins.rs_pin, 0);
-    lcd_clear(&lcd_pins);
-    lcd_home(&lcd_pins);
+
+//  Initialize LCD for control interface.
+  lcd_initialize(&reg_pins);
 
   // SPI baudrate
   uint32_t my_baudrate = 5000000;
@@ -185,162 +178,140 @@ int main() {
   uint64_t time_sent = 0; // time packet was sent
   uint64_t time_reply = 0; // response time after packet sent
 
+  initialize_print_debug(&my_config, &my_pins, my_baudrate);
 
 while(1) {
 
-  battery_status_to_shift(&u_vars, &pico_adc, &bat_status, 1);
-   write_display(&lcd_pins, bat_status.battery_level, bat_status.battery_printer);
-
+  printf("\n");
   adc_pin_call(&pico_adc);
+  u_vars.register_value_one = 0x07;
+
+  battery_status_to_shift(&u_vars, &pico_adc, &bat_status, 1);
   cycle_handler(&u_vars, &enabled_buttons, rgb_cycle);
   
   payload_buffer(&load_o,&pico_adc, &rotary, &u_vars);
-  serial_register_output(&reg_pins, &u_vars, 1, NULL);
 
-  sprintf(rf_function, "RF Switching");
-  sprintf(rf_transmitting, "Transmitting Now");
-   write_display(&lcd_pins, rf_function, rf_transmitting);
+  //  Begin Transmitting RF data.
 
-  for(int zv = 0; zv < 4; zv++){
 
-    switch(zv){
-
-      case 0:
-      sprintf(rf_channel, "Pipeline Zero.");
-            printf("Transmitter Switch Case 0.\n");
+  for(int zv = 0; zv < 1; zv++)
+  {
+    sprintf(rf_arr.rf_channel, "Pipeline Zero.");
+      printf("Pipeline 0.\n");
 
     // send to receiver's DATA_PIPE_0 address
-    my_nrf.tx_destination((uint8_t[]){0x37,0x37,0x37,0x37,0x37});
-
+      my_nrf.tx_destination((uint8_t[]){0x37,0x37,0x37,0x37,0x37});
+      printf("Step 1.\n");
     // time packet was sent
-    time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
+      time_sent = to_us_since_boot(get_absolute_time()); // time sent
+      printf("Step 2.\n");
     // send packet to receiver's DATA_PIPE_0 address
-    printf("\n Sending: 0b%08x.\n", load_o.payload_zero);
-    success = my_nrf.send_packet(&load_o.payload_zero, sizeof(load_o.payload_zero));
-    sprintf(rf_data, "Data Sent: %u.", load_o.payload_zero);
-
+      printf("\n Sending: 0b%04x.\n", load_o.payload_zero);
+      success = my_nrf.send_packet(&load_o.payload_zero, sizeof(load_o.payload_zero));
+      printf("Step 3.\n");
     // time auto-acknowledge was received
-    time_reply = to_us_since_boot(get_absolute_time()); // response time
-
+      time_reply = to_us_since_boot(get_absolute_time()); // response time
+      printf("Step 4.\n");
+      sprintf(rf_arr.rf_data, "Attempted: %u", load_o.payload_zero);
+      printf("Step 5.\n");
     if (success)
     {
-      printf("\nPacket sent:- Response: %lluμS | Payload: 0b%08x.\n", time_reply - time_sent, load_o.payload_zero);
-      write_display(&lcd_pins, rf_channel, rf_data);
+      printf("\nPacket sent:- Response: %lluμS | Payload: 0b%04x.\n", time_reply - time_sent, load_o.payload_zero);
+      //  lcd_screen_write(&reg_pins, com_char_e, rf_arr.rf_channel, rf_arr.rf_data);
 
-    } else {
+    } 
+    else
+    {
 
       printf("\nPacket not sent:- Receiver not available.\n");
+      //  lcd_screen_write(&reg_pins, com_char_e, rf_arr.error_char_t, rf_arr.rf_data);
 
-     write_display(&lcd_pins, error_char_t, rf_data);
     }
-        sleep_ms(300);
-
-        break;
-        case 1:
-        sprintf(rf_channel, "Pipeline One.");
-            printf("Transmitter Switch Case 1.\n");
-
+    
+    sprintf(rf_arr.rf_channel, "Pipeline One.");
+      printf("Pipeline 1.\n");
     // send to receiver's DATA_PIPE_1 address
-    my_nrf.tx_destination((uint8_t[]){0xC7,0xC7,0xC7,0xC7,0xC7});
-
+      my_nrf.tx_destination((uint8_t[]){0xC7,0xC7,0xC7,0xC7,0xC7});
     // time packet was sent
-    time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
+      time_sent = to_us_since_boot(get_absolute_time()); // time sent
     // send packet to receiver's DATA_PIPE_1 address
-    printf("\n Sending: 0b%08x.\n", load_o.payload_one_t.vertical_buffer);
-    printf("\n Sending: 0b%08x.\n", load_o.payload_one_t.horizontal_buffer);
-    success = my_nrf.send_packet(&load_o.payload_one_t, sizeof(load_o.payload_one_t));
-
-    sprintf(rf_data, "VD: %u HD: %u", load_o.payload_one_t.vertical_buffer, load_o.payload_one_t.horizontal_buffer);
-
+      printf("\n Sending: 0b%04x.\n", load_o.payload_one_t.vertical_buffer);
+      printf("\n Sending: 0b%04x.\n", load_o.payload_one_t.horizontal_buffer);
+      success = my_nrf.send_packet(&load_o.payload_one_t, sizeof(load_o.payload_one_t));\
     // time auto-acknowledge was received
-    time_reply = to_us_since_boot(get_absolute_time()); // response time
-
+      time_reply = to_us_since_boot(get_absolute_time()); // response time
+      sprintf(rf_arr.rf_data, "Vertical: %u", load_o.payload_one_t.vertical_buffer);
+      sprintf(rf_arr.rf_data_two, "Horizontal: %u", load_o.payload_one_t.horizontal_buffer);
     if (success)
     {
         printf("\nPacket sent:- Response: %lluμS | Payload: 0b%04x & 0b%04x.\n",time_reply - time_sent, load_o.payload_one_t.vertical_buffer, load_o.payload_one_t.horizontal_buffer);
-        write_display(&lcd_pins, rf_channel, rf_data);
-    } else {
+          lcd_screen_write(&reg_pins, com_char_e, rf_arr.rf_channel, rf_arr.rf_data);
+
+    }
+    else
+    {
 
       printf("\nPacket not sent:- Receiver not available.\n");
-      write_display(&lcd_pins, error_char_t, rf_data);
+        lcd_screen_write(&reg_pins, com_char_e, rf_arr.error_char_t, rf_arr.rf_data);
+
     }
-        sleep_ms(300);
-                
-        break;
-        case 2:
-
-        sprintf(rf_channel, "Pipeline Two.");
-                printf("Transmitter Switch Case 2.\n");
-
+          
+    sprintf(rf_arr.rf_channel, "Pipeline Two.");
+      printf("Pipeline 2.\n");
     // send to receiver's DATA_PIPE_2 address
-    my_nrf.tx_destination((uint8_t[]){0xC8,0xC7,0xC7,0xC7,0xC7});
-
+      my_nrf.tx_destination((uint8_t[]){0xC8,0xC7,0xC7,0xC7,0xC7});
     // time packet was sent
-    time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
+      time_sent = to_us_since_boot(get_absolute_time()); // time sent
     // send packet to receiver's DATA_PIPE_2 address
-    printf("\n Sending: 0b%04x.\n", load_o.payload_two);
-    success = my_nrf.send_packet(&load_o.payload_two, sizeof(load_o.payload_two));
-    sprintf(rf_data, "Payload: %u", load_o.payload_two);
-    
+      printf("\n Sending: 0b%04x.\n", load_o.payload_two);
+      success = my_nrf.send_packet(&load_o.payload_two, sizeof(load_o.payload_two));
+    sprintf(rf_arr.rf_data, "Attempted: %u", load_o.payload_two);
     // time auto-acknowledge was received
-    time_reply = to_us_since_boot(get_absolute_time()); // response time
-
+      time_reply = to_us_since_boot(get_absolute_time()); // response time
     if (success)
     {
       printf("\nPacket sent:- Response: %lluμS | Payload: 0b%04x.\n", time_reply - time_sent, load_o.payload_two);
-      write_display(&lcd_pins, rf_channel, rf_data);
+        lcd_screen_write(&reg_pins, com_char_e, rf_arr.rf_channel, rf_arr.rf_data);
 
-    }  else {
+    }
+    else
+    {
 
       printf("\nPacket not sent:- Receiver not available.\n");
-      write_display(&lcd_pins, error_char_t, rf_data);
+        lcd_screen_write(&reg_pins, com_char_e, rf_arr.error_char_t, rf_arr.rf_data);
     }
-        sleep_ms(300);
 
-        break;
-        case 3:
-
-        sprintf(rf_channel, "Pipeline Three.");
-        printf("Transmitter Switch Case 3.\n");
-
+    sprintf(rf_arr.rf_channel, "Pipeline Three.");
+      printf("Pipeline Three.\n");
     // send to receiver's DATA_PIPE_3 address
-    my_nrf.tx_destination((uint8_t[]){0xC9,0xC7,0xC7,0xC7,0xC7});
-
+      my_nrf.tx_destination((uint8_t[]){0xC9,0xC7,0xC7,0xC7,0xC7});
     // time packet was sent
-    time_sent = to_us_since_boot(get_absolute_time()); // time sent
-
+      time_sent = to_us_since_boot(get_absolute_time()); // time sent
     // send packet to receiver's DATA_PIPE_3 address
-    printf("\n Sending: 0b%08x.\n", load_o.payload_three);
-    success = my_nrf.send_packet(&load_o.payload_three, sizeof(load_o.payload_three));
-     sprintf(rf_data, "Payload: %u", load_o.payload_three);
-
+      printf("\n Sending: 0b%08x.\n", load_o.payload_three);
+      success = my_nrf.send_packet(&load_o.payload_three, sizeof(load_o.payload_three));
+    sprintf(rf_arr.rf_data, "Attempted: %u", load_o.payload_three);
     // time auto-acknowledge was received
-    time_reply = to_us_since_boot(get_absolute_time()); // response time
-
+      time_reply = to_us_since_boot(get_absolute_time()); // response time
     if (success)
     {
       printf("\nPacket sent:- Response: %lluμS | Payload: 0b%08x.\n", time_reply - time_sent, load_o.payload_three);
-      write_display(&lcd_pins, rf_channel, rf_data);
+        lcd_screen_write(&reg_pins, com_char_e, rf_arr.rf_channel, rf_arr.rf_data);
 
-    } else {
+    }
+    else
+    {
 
       printf("\nPacket not sent:- Receiver not available.\n");
-      write_display(&lcd_pins, error_char_t, rf_data);
+        lcd_screen_write(&reg_pins, com_char_e, rf_arr.error_char_t, rf_arr.rf_data);
     }
-        sleep_ms(300);
 
-        break;
-        default:
-            printf("Transmitter Switch Default.\n");
-            write_display(&lcd_pins, error_char_t, error_char_b);
-        break;
-        }
-    }
-   // sleep_ms(500);
+    }    
+  
 
+
+    sleep_ms(500);
+    printf("Next Iteration?\n");
 }
 tight_loop_contents();
 }
